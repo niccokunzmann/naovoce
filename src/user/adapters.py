e543ecb3257a4c8ff7b33b359c0c5ac01a38fef3
Ondra Nejedlý <jsmesami@gmail.com>
2018-09-10 23:03:51 +0200

    Employ Coala and PyLint

--- src/user/adapters.py
+++ src/user/adapters.py
@@ -1,17 +1,13 @@
-import smtplib
 import logging
-
-from django.core.mail import mail_managers
-from django.utils.translation import ugettext
-from django.contrib import messages
+import smtplib
+from user.models import FruitUser
 
 from allauth.account.adapter import DefaultAccountAdapter
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
-
+from django.contrib import messages
+from django.core.mail import mail_managers
+from django.utils.translation import ugettext
 from rest_framework.reverse import reverse
-
-from user.models import FruitUser
-
 
 logger = logging.getLogger(__name__)
 
@@ -19,9 +15,8 @@ logger = logging.getLogger(__name__)
 class AccountAdapter(DefaultAccountAdapter):
 
     def send_confirmation_mail(self, request, emailconfirmation, signup):
-        """
-        Do not just fail for typo in recipient email.
-        """
+        """Do not just fail for typo in recipient email."""
+
         try:
             super().send_confirmation_mail(request, emailconfirmation, signup)
         except smtplib.SMTPRecipientsRefused as e:
@@ -37,21 +32,16 @@ class AccountAdapter(DefaultAccountAdapter):
             mail_managers(error_msg, str(e))
 
     def add_message(self, request, *args, **kwargs):
-        """
-        Do not use 'django.contrib.messages' if used by API.
-        """
+        """Do not use 'django.contrib.messages' if used by API."""
         if not request.path.startswith(reverse('api:root')):
             super().add_message(request, *args, **kwargs)
 
 
 class SocialAccountAdapter(DefaultSocialAccountAdapter):
-    """
-    Allauth account adapter that automatically connects social accounts to existing accounts.
-    """
+    """Allauth account adapter that automatically connects social accounts to existing accounts."""
+
     def pre_social_login(self, request, sociallogin):
-        """
-        It there is an existing user, connect him to an account with the same email.
-        """
+        """It there is an existing user, connect him to an account with the same email."""
         # If social account exists, just proceed.
         if sociallogin.is_existing:
             return
